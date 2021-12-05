13ea433056984170fd3d9499e71aa96735caa646
QQmberling <67502319+QQmberling@users.noreply.github.com>
2021-12-06 02:38:43 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,53 @@
+import datetime
+
+from flask import Flask, request, render_template
+from flask_wtf import FlaskForm
+from wtforms import DateField, SubmitField
+from wtforms.validators import DataRequired
+
+
+REFERENCE_DATE = datetime.date(2018, 12, 30)
+
+
+class DateForm(FlaskForm):
+    date = DateField(validators=[DataRequired()])
+    submit = SubmitField("Узнать")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.date.data:
+            self.date.data = datetime.date.today()
+
+
+def get_week_number(desire_date, reference_date=REFERENCE_DATE):
+    """
+    :param desire_date: Желаемая дата, для которой надо узнать номер недели
+    :param reference_date: Дата отсчета
+    :return: week_number: Номер недели
+    """
+    amount_of_days = (desire_date - reference_date).days
+    if amount_of_days < 0:
+        return 1
+    return amount_of_days//7 + 1
+
+
+app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = "you will never guess"
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    form = DateForm()
+    date = datetime.date.today()
+    if request.method == 'GET':
+        form = DateForm()
+    elif form.validate_on_submit():
+        date = form.date.data
+    else:
+        return render_template('index.html', form=form, week_number=None)
+    week_number = get_week_number(date)
+    return render_template('index.html', form=form, week_number=week_number)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ tests/test_get_week_number.py
@@ -0,0 +1,37 @@
+import datetime
+
+from run import get_week_number
+from datetime import date
+
+AMOUNT_WEEKS = 5000
+
+
+def test_oldest_dates():
+    """ Тест дат до начала первой недели
+    """
+    assert get_week_number(date(1, 1, 1)) == 1
+    assert get_week_number(date(2018, 12, 29)) == 1
+
+
+def test_first_week():
+    for i in range(7):
+        assert get_week_number(date(2018, 12, 30) + datetime.timedelta(days=i)) == 1
+
+
+def test_second_week():
+    for i in range(7):
+        assert get_week_number(date(2018, 12, 30) + datetime.timedelta(days=7+i)) == 2
+
+
+def test_every_week(amount=AMOUNT_WEEKS):
+    """ Тест {amount} недель после начальной даты
+    """
+    for week_num in range(1, amount+1):
+        ith_week(week_num)
+
+
+def ith_week(week_num):
+    """ Тест {week_num} недели по счету
+    """
+    for i in range(7):
+        assert get_week_number(date(2018, 12, 30) + datetime.timedelta(days=(week_num - 1) * 7 + i)) == week_num
